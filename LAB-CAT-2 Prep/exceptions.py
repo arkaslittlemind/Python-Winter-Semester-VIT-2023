e974be27fd24afa08d57079693c062b725e88250
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-22 22:52:47 +0530

    master

--- LAB-CAT-2 Prep/exceptions.py
+++ LAB-CAT-2 Prep/exceptions.py
@@ -1,11 +1,24 @@
-a = input("Enter a number: ")
-print(f"Multiplication table of {a} is: ")
+##a = input("Enter a number: ")
+##print(f"Multiplication table of {a} is: ")
+
+##try:
+    ##for i in range(1, 11):
+        ##print(f"{int(a)} x {i} = {int(a) * i}") 
+
+##except ValueError:
+    ##print("Cant multiply string with int")
+    
+##print("End of program")
 
 try:
-    for i in range(1, 11):
-        print(f"{int(a)} x {i} = {int(a) * i}") 
-
-except:
-    print("Cant multiply string with int")
+    num = int(input("Enter an integer: "))
+    a = [6, 3]
+    print(a[num])
     
-print("End of program") 
+except ValueError:
+    print("You must enter an integer")
+    
+
+except IndexError:
+    print("Index out of range")
+    a = [6, 3]

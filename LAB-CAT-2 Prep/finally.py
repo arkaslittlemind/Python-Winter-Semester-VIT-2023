8abd09b67bdb689d67bd093cc418da36bfa463c6
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-23 00:12:16 +0530

    master

--- LAB-CAT-2 Prep/finally.py
+++ LAB-CAT-2 Prep/finally.py
@@ -1,10 +1,19 @@
-try:
-    l = [1, 5, 6, 7]
-    i = int(input("Enter the index: "))
-    print(l[i])
+def func1():
     
-except IndexError:
-    print("Index out of range")
+    try:
+        l = [1, 5, 6, 7]
+        i = int(input("Enter the index: "))
+        print(l[i])
+        return 1
     
-finally:
-    print("Finally block is always executed")
+    except IndexError:
+        print("Index out of range")
+        return 0
+    
+    ##finally:
+        ##print("Finally block is always executed")
+    
+    print("End of program")
+        
+x = func1()
+print(x)

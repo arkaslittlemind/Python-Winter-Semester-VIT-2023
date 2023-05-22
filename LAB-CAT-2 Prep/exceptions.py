5ee3dd0e45e936c91387b2120798789969541ae2
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-05-22 22:42:52 +0530

    master

--- LAB-CAT-2 Prep/exceptions.py
+++ LAB-CAT-2 Prep/exceptions.py
@@ -1,5 +1,11 @@
 a = input("Enter a number: ")
 print(f"Multiplication table of {a} is: ")
 
-for i in range(1, 11):
-    print(f"{int(a)} x {i} = {int(a) * i}") 
+try:
+    for i in range(1, 11):
+        print(f"{int(a)} x {i} = {int(a) * i}") 
+
+except Exception as e:
+    print(e)
+    
+print("End of program")
